9eea2126e85a4f81682a89eeeca41c0236d0c700
超 <1286439347@qq.com>
2018-06-15 17:19:28 +0800

    a

--- exTest/koaMain.js
+++ exTest/koaMain.js
@@ -1,20 +1,40 @@
 const Koa = require('koa')
 const router = require('koa-router')();
 const link = require("./middleware/logger-generator.js")
+const cors = require('koa2-cors');
+var bodyParser = require('koa-bodyparser');
 const app = new Koa()
+app.use(cors())
+app.use(bodyParser())
 app.use(router.routes());
-router.post('/abcd', async (ctx) => {
-    let D = ''
-    await link.PostRequest(ctx.query)
-    .then(res => {
-        D = res
-    })
-    console.log(111,D)
-    // let url = ctx.url
-    // let ctx_query = ctx.query
-    // let ctx_querystring = ctx.querystring
-    ctx.body = D
+let postD = ''
+let getD = ''
+async function postReq(data, url) {
+    await link.PostRequest(data, url)
+        .then(res => {
+            postD = res
+        })
+}
+async function getReq(data, url) {
+    await link.GetRequest(data, url)
+        .then(res => {
+            getD = res
+        })
+}
+app.use(async (ctx) => {
+    if (ctx.method === "POST") {
+        await postReq(ctx.request.body, ctx.url)
+        ctx.body = postD
+    } else if (ctx.method === "GET") {
+        await getReq(ctx.request.body, ctx.url)
+        ctx.body = getD
+    }
 })
+
+// router.post('/org/query_site_name', async (ctx) => {
+//     await postReq(ctx.request.body, ctx.url)
+//     ctx.body = D
+// })
 // app.use(async (ctx) => {
 //     // // 从上下文的request对象中获取
 //     // let request = ctx.request
--- exTest/middleware/logger-generator.js
+++ exTest/middleware/logger-generator.js
@@ -10,15 +10,36 @@ var request = require('superagent');
 // var headers = {
 //   'Content-Type': 'application/json',
 // };
-var PostRequest = (D)=> {
+// var U = 'http://10.163.126.142:8080/user'
+var U = 'http://192.168.1.16:8080/user'
+var PostRequest = (D, url) => {
+  console.log(D)
   return request
-    .post('http://10.163.126.142:8080/user/org/query_site_name')
-    .withCredentials()
+    .post(`${U + url}`)
     .send(D)
     .then(res => {
       if (res.status === 200) {
         return JSON.parse(res.text)
       }
     })
+    .catch(err => {
+      console.log('aaaaaaaaaa')
+      return { code: 404, msg: '未知异常' }
+    })
+}
+var GetRequest = (D, url) => {
+  return request
+    .post(`${U + url}`)
+    .query(D)
+    .then(res => {
+      if (res.status === 200) {
+        return JSON.parse(res.text)
+      }
+    })
+    .catch(err => {
+      console.log('aaaaaaaaaa')
+      return { code: 404, msg: '未知异常' }
+    })
 }
 exports.PostRequest = PostRequest;
+exports.GetRequest = GetRequest;
