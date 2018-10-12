035e5097afa19bacf487a46c5c1ab6d28dbdb4e6
超 <1286439347@qq.com>
2018-10-12 16:45:01 +0800

    cc

--- exTest/index.js
+++ exTest/index.js
@@ -13,12 +13,12 @@ app.use(
     multipart: true, // 支持文件上传
     encoding: "utf-8",
     formidable: {
-      uploadDir: path.join(__dirname, "upload/"), // 设置文件上传目录
+      uploadDir: path.join(__dirname, "static/"), // 设置文件上传目录
       keepExtensions: true, // 保持文件的后缀
       maxFileSize: 2 * 1024 * 1024, // 文件上传大小
       onFileBegin: (name, file) => {
         // 文件上传前的设置
-        file.path = "upload/" + file.name; //修改文件名字
+        // file.path = "upload/" + file.name; //修改文件名字
         console.log(`name: ${name}`);
       },
       onError:(err)=>{
@@ -43,19 +43,7 @@ app.use(async ctx => {
       // return (ctx.body = "上传成功！");
     } else if (ctx.url === "/uploadfiles") {
       // 上传多个文件
-      const files = ctx.request.files.file; // 获取上传文件
-      for (let file of files) {
-        // 创建可读流
-        const reader = fs.createReadStream(file.path);
-        // 获取上传文件扩展名
-        let filePath = path.join(__dirname, "public/upload/") + `/${file.name}`;
-        // 创建可写流
-        const upStream = fs.createWriteStream(filePath);
-        // 可读流通过管道写入可写流
-        reader.pipe(upStream);
-      }
-      ctx.body = { res: 0, msg: "上传成功！" };
-      // return (ctx.body = "上传成功！");
+      
     } else {
       let POST_D = await link.PostRequest(ctx.request.body, ctx.url);
       ctx.body = POST_D;
